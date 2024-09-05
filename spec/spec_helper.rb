3b4b39ea5c45245c24125122587707cb5ff370f7
rubycidev <117899216+rubycidev@users.noreply.github.com>
2024-09-05 22:01:48 +0200

    Update spec_helper.rb script

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
+if ENV["RUBY_CI_SECRET_KEY"]
+  require "rspec/core/runner"
+  require "ruby_ci/runner_prepend"
+
+  class RSpec::Core::ExampleGroup
+    def self.filtered_examples
+      rubyci_scoped_ids = Thread.current[:rubyci_scoped_ids] || ""
+
+      RSpec.world.filtered_examples[self].filter do |ex|
+        rubyci_scoped_ids == "" || /^#{rubyci_scoped_ids}($|:)/.match?(ex.metadata[:scoped_id])
+      end
+    end
+  end
+
+  RSpec::Core::Runner.prepend(RubyCI::RunnerPrepend)
+end
 
 require 'minitest/spec'
 require 'simplecov'
